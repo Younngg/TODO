d9d87ae78bb34747cb92775fc267cc386a822a71
DESKTOP-8JMU16V\Administrator <8533283@naver.com>
2022-05-15 20:10:50 +0900

    4.5

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
-const h1 = document.querySelector(".hello h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleh1Click(){
-    h1.style.color = "blue";
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!"
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!"
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleCopy() {
-    alert("copy?");
-}
-
-function handleOffline() {
-    alert("WIFI OFFLINE")
-}
-
-h1.onclick = handleh1Click;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleCopy);
-window.addEventListener("offline", handleOffline)
+loginForm.addEventListener("submit", onLoginSubmit);
