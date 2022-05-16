5a667fb090af25ec8575c98cc15aa304218fb70a
Younngg <8533283@naver.com>
2022-05-16 22:03:26 +0900

    assignment 02 finished

--- assignments/2/src/index.js
+++ assignments/2/src/index.js
@@ -1,23 +1,18 @@
-const numForm = document.querySelector("#number-form")
-const numRange = document.querySelector("#number-range")
-const numUser = document.querySelector("#number-user")
-const submitBtn = document.querySelector("#submit-btn")
-const gameResult = document.querySelector("#game-result")
-
-const HIDDEN_CLASSNAME = "hidden";
-
+const numForm = document.querySelector("#number-form");
+const numRange = document.querySelector("#number-range");
+const numUser = document.querySelector("#number-user");
+const gameResult = document.querySelector("#game-result");
 
 function onNumberSubmit(event) {
     event.preventDefault();
     const num = numUser.value;
-    localStorage.setItem("num", num);
-    gameResult.innerHTML = `You choose: ${numUser.value}, the machine choose: `;
+    const random = Math.ceil(Math.random() * numRange.value);
+    if(num != random) {
+        gameResult.innerHTML = `You chose: ${num}, the machine chose: ${random}.<br>You lost!`;
+    } else {
+        gameResult.innerHTML = `You chose: ${num}, the machine chose: ${random}.<br>You won!`;
+    }
+    
 }
 
-numForm.addEventListener("submit", onNumberSubmit)
-
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+numForm.addEventListener("submit", onNumberSubmit);
